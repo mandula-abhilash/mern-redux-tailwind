e12af74dd8d6b50fa6b5211f02d1c4fc9c2180a9
Abhilash Mandula <mandula.abhilash@gmail.com>
2021-01-17 17:08:58 +0530

    Initial commit - add folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex bg-blue-200 min-h-screen">
+      <h1 className="mx-auto my-auto text-2xl">
+        MERN + Tailwind Boiler plate code
+      </h1>
+    </div>
+  );
+}
+
+export default App;
