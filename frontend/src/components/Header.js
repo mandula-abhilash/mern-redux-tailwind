28677679748cb42e07beb681640cedec11e5702b
Abhilash Mandula <mandula.abhilash@gmail.com>
2021-01-21 09:39:07 +0530

    Fix header css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,14 +13,14 @@ const Header = () => {
   };
 
   return (
-    <div className="mb-2 p-6 md:flex items-center w-full justify-between mx-auto container">
+    <div className="m-2 p-2 md:flex items-center w-full justify-between mx-auto container">
       <div className="flex md:w-1/2 justify-between items-center">
         <div className="rounded relative w-8/12 md:w-10/12 lg:w-1/2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="text-gray-500 absolute ml-4 inset-0 m-auto icon icon-tabler icon-tabler-search"
-            width="20"
-            height="20"
+            className="text-gray-500 absolute ml-4 inset-0 m-auto icon icon-tabler icon-tabler-search border-r-1 border-gray-900"
+            width="16"
+            height="16"
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
@@ -33,15 +33,23 @@ const Header = () => {
             <line x1="21" y1="21" x2="15" y2="15"></line>
           </svg>
           <input
-            className="border border-gray-100 focus:outline-none focus:border-brand rounded-full w-full text-sm bg-gray-100 text-gray-500 pl-12 sm:pl-10 md:pl-12 py-3 pr-4"
+            className="border border-gray-100 focus:outline-none focus:border-brand rounded-full w-full text-sm bg-gray-100 text-gray-500 pl-12 sm:pl-10 md:pl-12 py-2 pr-4"
             type="text"
             placeholder="Search"
           />
         </div>
-        <div className="flex items-center md:hidden ml-6 md:ml-0">
-          <div className="mr-4 md:mr-6 cursor-pointer" onClick={logoutHandler}>
+        <div className="flex items-center md:hidden mr-4">
+          <Link to="/profile">
+            <img
+              className="h-8 w-8 md:h-10 md:w-10"
+              src="https://cdn.tuk.dev/assets/components/todos/profile.png"
+              alt="profile"
+              srcSet=""
+            />
+          </Link>
+          <div className="ml-2 md:ml-0 cursor-pointer" onClick={logoutHandler}>
             <svg
-              className="h-8 w-8 text-blue-800"
+              className="h-6 lg:h-8 w-6 lg:w-8 text-blue-800"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 64 64"
             >
@@ -59,14 +67,6 @@ const Header = () => {
               />
             </svg>
           </div>
-          <Link to="/profile">
-            <img
-              className=""
-              src="https://cdn.tuk.dev/assets/components/todos/profile.png"
-              alt="profile"
-              srcSet=""
-            />
-          </Link>
         </div>
       </div>
 
@@ -95,10 +95,18 @@ const Header = () => {
           </svg>
         </div>
       </div> */}
-        <div className="ml-4 lg:ml-12 hidden md:flex items-center">
-          <div className="mr-2 lg:mr-6 cursor-pointer" onClick={logoutHandler}>
+        <div className="mr-2 lg:mr-6 hidden md:flex items-center">
+          <Link to="/profile">
+            <img
+              className="h-8 w-8 md:h-10 md:w-10"
+              src="https://cdn.tuk.dev/assets/components/todos/profile.png"
+              alt="profile"
+              srcSet=""
+            />
+          </Link>
+          <div className="ml-4 lg:ml-6 cursor-pointer" onClick={logoutHandler}>
             <svg
-              className="h-10 w-10 text-blue-800 font-bold"
+              className="h-6 lg:h-8 w-6 lg:w-8 text-blue-800"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 64 64"
             >
@@ -116,13 +124,6 @@ const Header = () => {
               />
             </svg>
           </div>
-          <Link to="/profile">
-            <img
-              src="https://cdn.tuk.dev/assets/components/todos/profile.png"
-              alt="profile"
-              srcSet=""
-            />
-          </Link>
         </div>
       </div>
     </div>
