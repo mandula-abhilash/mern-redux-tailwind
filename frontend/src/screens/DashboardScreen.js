ccfc289fe91a1fb4a9d90ad84aa1f08e3d0c2342
Abhilash Mandula <mandula.abhilash@gmail.com>
2021-01-28 20:20:12 +0530

    Add dashboard screen

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
+import Header1 from "../components/Header1";
 
 const DashboardScreen = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Header1 />
       <div className="flex mx-auto h-screen bg-indigo-100 justify-center items-center">
         Dashboard Screen
       </div>
