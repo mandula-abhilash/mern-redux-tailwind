54229f330b420f5022a361009969ae6386b7eba2
Abhilash Mandula <mandula.abhilash@gmail.com>
2021-04-10 19:43:07 +0530

    Add 404 page not found screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import DashboardScreen from "./screens/DashboardScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
@@ -16,22 +16,25 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Router>
-        <AdminRoute exact path="/admin/users" component={UserListScreen} />
-        <AdminRoute
-          exact
-          path="/council/:id/edit"
-          component={EditCouncilScreen}
-        />
-        <PrivateRoute exact path="/dashboard" component={DashboardScreen} />
-        <PrivateRoute
-          exact
-          path="/auth/activate/:token"
-          component={ActivateScreen}
-        />
-        <PrivateRoute exact path="/profile" component={ProfileScreen} />
-        <Route exact path="/register" component={RegisterScreen} />
-        <Route exact path="/test" component={TestScreen} />
-        <Route exact path="/login" component={LoginScreen} />
+        <Switch>
+          <AdminRoute exact path="/admin/users" component={UserListScreen} />
+          <AdminRoute
+            exact
+            path="/council/:id/edit"
+            component={EditCouncilScreen}
+          />
+          <PrivateRoute exact path="/dashboard" component={DashboardScreen} />
+          <PrivateRoute
+            exact
+            path="/auth/activate/:token"
+            component={ActivateScreen}
+          />
+          <PrivateRoute exact path="/profile" component={ProfileScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <Route exact path="/test" component={TestScreen} />
+          <Route exact path="/" component={LoginScreen} />
+          <Route component={Screen404} />
+        </Switch>
         <Footer />
       </Router>
     </div>
